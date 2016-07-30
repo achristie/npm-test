972b3ffa2b720a0e969bc36f6d5195251913b726
Andrew Christie <awchristie86@gmail.com>
2016-07-29 23:02:24 -0400

    'test'

---
+++ src/index.test.js
@@ -0,0 +1,8 @@
+var expert = require('chai').expect;
+var starWars = require('./index');
+
+describe('starwars-names', function () {
+  it('should work', function () {
+    expect(true).to.be.true
+  });
+});
